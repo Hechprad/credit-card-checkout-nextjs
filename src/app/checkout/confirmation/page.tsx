5984eebac9d3eabab1c22f77c2751df81bd4a7fa
Hecherat <hecherat@gmail.com>
2025-03-07 23:52:54 -0300

    feat: Add Checkout pages and new routes

--- src/app/checkout/confirmation/page.tsx
+++ src/app/checkout/confirmation/page.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+export default function Confirmation() {
   return (
     <div className='flex flex-col justify-start items-center w-[325px] h-screen p-[20px]'>
-      <h1 className=' text-[20px] text-white-1  mb-[32px]'>Home</h1>
+      <h1 className=' text-[20px] text-white-1  mb-[32px]'>
+        Confirmação de compra
+      </h1>
       <Button>
-        <Link href='/checkout/payment'>Ir para checkout</Link>
+        <Link href='/checkout/payment'>Ir para Checkout</Link>
       </Button>
     </div>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+export default function CheckoutNotFound() {
   return (
     <div className='flex flex-col justify-start items-center w-[325px] h-screen p-[20px]'>
-      <h1 className=' text-[20px] text-white-1  mb-[32px]'>Home</h1>
+      <h1 className=' text-[20px] text-white-1  mb-[32px]'>
+        Página não encontrada
+      </h1>
       <Button>
         <Link href='/checkout/payment'>Ir para checkout</Link>
       </Button>
