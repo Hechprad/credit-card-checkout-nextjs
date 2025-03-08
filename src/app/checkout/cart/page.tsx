5dc01d490f431df8ca8e997eba456dbf866323cd
Hecherat <hecherat@gmail.com>
2025-03-07 23:52:54 -0300

    feat: Add Checkout pages and new routes

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+export default function Cart() {
   return (
     <div className='flex flex-col justify-start items-center w-[325px] h-screen p-[20px]'>
-      <h1 className=' text-[20px] text-white-1  mb-[32px]'>Home</h1>
+      <h1 className=' text-[20px] text-white-1  mb-[32px]'>Carrinho</h1>
       <Button>
-        <Link href='/checkout/payment'>Ir para checkout</Link>
+        <Link href='/checkout/payment'>Ir para Checkout</Link>
       </Button>
     </div>
   );
