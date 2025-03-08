1a62191dba858148978ba0a6431e0d354dc9df25
Hecherat <hecherat@gmail.com>
2025-03-08 12:41:48 -0300

    feat: Add Zod schema and falidation function

--- src/components/CreditCardForm/index.tsx
+++ src/components/CreditCardForm/index.tsx
@@ -1,3 +1,8 @@
+import { Controller, SubmitErrorHandler, SubmitHandler } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Select } from '@/components/ui/select';
 import {
   Tooltip,
   TooltipContent,
@@ -5,48 +10,109 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Select } from '@/components/ui/select';
-
 import InfoIcon from '@/assets/icons/information.svg';
 
-export default function CreditCardForm() {
+import * as h from './helpers';
+import * as t from './types';
+
+export default function CreditCardForm({
+  control,
+  errors,
+  handleSubmit,
+  register,
+}: t.CreditCardFormProps) {
+  const onSubmit: SubmitHandler<t.creditCardFormFields> = (data) =>
+    console.log(data);
+  const onSubmitError: SubmitErrorHandler<t.creditCardFormFields> = (data) =>
+    console.log(data);
+
   return (
-    <form id='' className='flex flex-col gap-[42px] justify-start items-end'>
-      <Input
-        label='Número do cartão'
-        placeholder='Número do cartão'
-        type='text'
+    <form
+      id='credit-card-form'
+      className='flex flex-col gap-[42px] justify-start items-end'
+      onSubmit={handleSubmit(onSubmit, onSubmitError)}
+    >
+      <Controller
+        name='cardNumber'
+        control={control}
+        render={({ field }) => (
+          <Input
+            label='Número do cartão'
+            placeholder='Número do cartão'
+            type='text'
+            {...field}
+            maxLength={19}
+            onChange={(e) => field.onChange(h.formatCardNumber(e.target.value))}
+            errorMessage={errors.cardNumber?.message}
+          />
+        )}
       />
-      <Input
-        label='Nome (igual ao cartão)'
-        placeholder='Nome (igual ao cartão)'
-        type='text'
+      <Controller
+        name='name'
+        control={control}
+        render={({ field }) => (
+          <Input
+            label='Nome (igual ao cartão)'
+            placeholder='Nome (igual ao cartão)'
+            type='text'
+            {...field}
+            maxLength={30}
+            onChange={(e) => field.onChange(h.formatName(e.target.value))}
+            errorMessage={errors.name?.message}
+          />
+        )}
       />
+
       <div className='flex gap-[30px] w-full'>
-        <Input label='Validade' placeholder='Validade' type='text' />
-        <Input
-          label={
-            <>
-              <span className='w-auto size-[12px] text-gray-1'>CVV</span>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <InfoIcon className='size-[14px] fill-gray-1' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>
-                      O CVV é o código de segurança do seu cartão, geralmente
-                      com 3 dígitos, localizado no verso do cartão.
-                    </p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </>
-          }
-          placeholder='CVV'
-          type='text'
+        <Controller
+          name='expiryDate'
+          control={control}
+          render={({ field }) => (
+            <Input
+              label='Validade'
+              placeholder='Validade'
+              type='text'
+              {...field}
+              maxLength={5}
+              onChange={(e) =>
+                field.onChange(h.formatExpiryDate(e.target.value))
+              }
+              errorMessage={errors.expiryDate?.message}
+            />
+          )}
+        />
+        <Controller
+          name='cvv'
+          control={control}
+          render={({ field }) => (
+            <Input
+              label={
+                <>
+                  <span className='w-auto size-[12px] text-gray-1'>CVV</span>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <InfoIcon className='size-[14px] fill-gray-1' />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>
+                          O CVV é o código de segurança do seu cartão,
+                          geralmente com 3 dígitos, localizado no verso do
+                          cartão.
+                        </p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </>
+              }
+              placeholder='CVV'
+              type='text'
+              {...field}
+              maxLength={4}
+              onChange={(e) => field.onChange(h.formatCVV(e.target.value))}
+              errorMessage={errors.cvv?.message}
+            />
+          )}
         />
       </div>
       <Select
@@ -56,9 +122,11 @@ export default function CreditCardForm() {
           label: `${i + 1} x`,
           value: `${i + 1}`,
         }))}
+        {...register('installments')}
+        errorMessage={errors.installments?.message}
       />
 
-      <Button type='submit' className='mt-[20px]'>
+      <Button type='submit' form='credit-card-form' className='mt-[20px]'>
         Continuar
       </Button>
     </form>
---
+++ src/components/CreditCardForm/types.ts
@@ -0,0 +1,18 @@
+import {
+  Control,
+  FieldErrors,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from 'react-hook-form';
+import { z } from 'zod';
+
+import { creditCardSchema } from '@/components/CreditCardForm/helpers';
+
+export type creditCardFormFields = z.infer<typeof creditCardSchema>;
+
+export type CreditCardFormProps = {
+  control: Control<creditCardFormFields>;
+  errors: FieldErrors<creditCardFormFields>;
+  handleSubmit: UseFormHandleSubmit<creditCardFormFields>;
+  register: UseFormRegister<creditCardFormFields>;
+};
